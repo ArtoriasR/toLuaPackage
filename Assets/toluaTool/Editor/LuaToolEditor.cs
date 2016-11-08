800c423588ebf4a21f8feb9d6dee37c9abbad41f
ArtoriasR <mhntfs@163.com>
2016-11-08 17:08:15 +0800

    update

--- Assets/toluaTool/Editor/LuaToolEditor.cs
+++ Assets/toluaTool/Editor/LuaToolEditor.cs
@@ -69,6 +69,9 @@ public class LuaToolEditor
     {
         Debug.Log("CreateAutoComplete");
         AutoCompleteExport.Clear();
+
+        ExportSetting.ExportNameSpace = true;
+        ExportSetting.ZipFile = false;
         
         if (EditorApplication.isCompiling)
         {
